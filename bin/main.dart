b3392c12f881d91b671c1b9832b9ac64c8eb84ca
thunder775 <rahulmasih61@gmail.com>
2019-12-06 09:21:24 +0530

    initial

--- bin/main.dart
+++ bin/main.dart
@@ -4,9 +4,30 @@
 /// https://assets.leetcode.com/uploads/2018/10/22/rainwatertrap.png
 // The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
 // In this case, 6 units of rain water (blue section) are being trapped.
+import 'dart:math';
 
 // Example:
 // Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 // Output: 6
+int trappedWater(List<int> elevations) {
+  int maximum = elevations.reduce(max);
+  while (elevations.any((element) => element != 0)) {
+    List<int> maxIndices = [];
+    elevations.forEach((element) {
+      if (element == maximum) {
+        maxIndices.add(elevations.indexOf(element));
+        if (element!=0) {
+          element--;
+          print(element);
+        }
+      }
+    });
+    maximum--;
+  }
+}
+
 main() {
+//  trappedWater([0,2,0,3,2,4]);
+//  trappedWater([0,1,0,2,1,0,1,3,2,1,2,1]);
+  trappedWater([3, 0, 1, 2, 0, 1]);
 }
--- test/day_44_morning_challenge_test.dart
+++ test/day_44_morning_challenge_test.dart
@@ -1,6 +1,11 @@
 import 'package:test/test.dart';
-
+import '../bin/main.dart';
 void main() {
   test('Test Case 1', () {
+  });
+  test('trapped water', (){
+    expect(trappedWater([0,2,3,2,4]), 1);
+    expect(trappedWater([0,1,0,2,1,0,1,3,2,1,2,1]), 6);
+
   });
 }
